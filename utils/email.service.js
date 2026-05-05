30267d55b8623cf590bc9f32d6c623574d041986
ujwal282 <162153205+ujwal282@users.noreply.github.com>
2026-05-05 18:45:21 +0545

    Update email.service.js

--- utils/email.service.js
+++ utils/email.service.js
@@ -9,9 +9,10 @@ const createTransporter = () => {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
     },
-    tls: {
-      rejectUnauthorized: false,
-    },
+    family:4
+    // tls: {
+    //   rejectUnauthorized: false,
+    // },
   });
 };
 
