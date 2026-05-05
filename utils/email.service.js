424f9c513c54c56920ac6b2c8e5ff9c4bdb0609f
ujwal282 <162153205+ujwal282@users.noreply.github.com>
2026-05-05 18:19:36 +0545

    Update email.service.js

--- utils/email.service.js
+++ utils/email.service.js
@@ -4,7 +4,7 @@ const createTransporter = () => {
   return nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
-    secure: false,
+    secure: true,
       family: 4,
     auth: {
       user: process.env.EMAIL_USER,
